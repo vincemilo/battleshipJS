fc2eb9a55ad7191afa69f636718871c949fef8b1
Vince Milo <1554999+vincemilo@users.noreply.github.com>
2024-04-14 15:24:58 -0600

    fixed computer ai logic to keep track of old adjacent attacks and try them first before going back to random

--- src/checkAdjacent.js
+++ src/checkAdjacent.js
@@ -1,6 +1,5 @@
 import translateCoords from './translateCoords';
 import generateOptions from './generateOptions';
-import validateAttack from './validateAttack';
 
 const prevOptions = {};
 
@@ -8,23 +7,20 @@ export default function checkAdjacent(hitCoords, missCoords) {
     let adjSquare = '';
     hitCoords.forEach((e) => {
         const translated = translateCoords(e);
-        console.log(translated);
-        console.log(missCoords);
         const options = generateOptions(translated);
         // check if option has already been generated or is a miss
         options.forEach((option) => {
             if (!(option in prevOptions) && !(option in missCoords))
                 prevOptions[option] = null;
         });
-        //        const prevOptionsKeys = Object.keys(prevOptions);
-        // prevOptionsKeys.forEach((key) => {
-        //     if (key in missCoords) {
-        //         delete prevOptions[key];
-        //     } else {
-        //         console.log([key[0], key[2]]);
-        //     }
-        // });
-        adjSquare = validateAttack(options, missCoords);
+        const prevOptionsKeys = Object.keys(prevOptions);
+        prevOptionsKeys.forEach((key) => {
+            if (key in missCoords) {
+                delete prevOptions[key];
+            } else {
+                adjSquare = [Number(key[0]), Number(key[2])];
+            }
+        });
     });
     return adjSquare;
 }
